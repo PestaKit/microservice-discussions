b7ed30b5beafbc22efc5a06800e65120abb0d5ce
nums <bandido18cls15colombiano@hotmail.com>
2017-11-15 13:38:30 +0100

    refactore spec

--- swagger/discussions-specs/src/test/java/io/pestakit/discussions/api/spec/helpers/Environment.java
+++ swagger/discussions-specs/src/test/java/io/pestakit/discussions/api/spec/helpers/Environment.java
@@ -16,7 +16,7 @@ public class Environment {
     public Environment() throws IOException {
         Properties properties = new Properties();
         properties.load(this.getClass().getClassLoader().getResourceAsStream("environment.properties"));
-        String url = properties.getProperty("io.avalia.fruits.server.url");
+        String url = properties.getProperty("io.pestakit.discussions.server.url");
         api.getApiClient().setBasePath(url);
 
     }
