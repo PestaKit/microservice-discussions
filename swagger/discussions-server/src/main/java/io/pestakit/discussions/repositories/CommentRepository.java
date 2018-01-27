7f7800bb9b55a3e3edc774039f64d8c3e2222e15
asuto4ka <anastasia.zharkova@heig-vd.ch>
2018-01-27 20:00:48 +0100

    BD + Modif API Controller

--- swagger/discussions-server/src/main/java/io/pestakit/discussions/repositories/CommentRepository.java
+++ swagger/discussions-server/src/main/java/io/pestakit/discussions/repositories/CommentRepository.java
@@ -1,9 +1,12 @@
+/**
+File which containts interface commentRepository 
+*/
 package io.pestakit.discussions.repositories;
 
 import io.pestakit.discussions.entities.CommentEntity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CommentRepository extends CrudRepository<CommentEntity, Integer>{
-
+    //allows to find all comment using its ID    
     public CommentEntity findByIdComment(int id_comment);
 }
