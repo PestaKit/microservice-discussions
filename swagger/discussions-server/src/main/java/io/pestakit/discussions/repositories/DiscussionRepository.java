1bfbee9bbbb41b2e2f6aef65114ffc55b55cd50f
asuto4ka <anastasia.zharkova@heig-vd.ch>
2018-01-27 20:38:50 +0100

    BD + Modif API Controller

--- swagger/discussions-server/src/main/java/io/pestakit/discussions/repositories/DiscussionRepository.java
+++ swagger/discussions-server/src/main/java/io/pestakit/discussions/repositories/DiscussionRepository.java
@@ -1,3 +1,6 @@
+/**
+File which containts interface DiscussionRepository 
+*/
 package io.pestakit.discussions.repositories;
 
 import io.pestakit.discussions.entities.CommentEntity;
@@ -5,6 +8,6 @@ import io.pestakit.discussions.entities.DiscussionEntity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface DiscussionRepository extends CrudRepository<DiscussionEntity, Integer>{
-
+     //allows to find all discussion using its ID  
     public DiscussionEntity findByIdDiscussion(int id_discussion);
 }
